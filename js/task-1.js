7043583d2947e3b98c1aee011fda5de225ea5ba3
Sergiy V <sergiykv@gmail.com>
2025-01-23 21:53:36 +0100

    Homework

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,18 @@
 'use strict';
 console.log('==================== Task 1');
 
-function slugify(title) {
-  // It could be done with regular expression faster :-)
-  // return title.toLowerCase().replace(/\s+/g, '-');
-  return title.toLowerCase().split(' ').join('-');
+function isEnoughCapacity(products, containerSize) {
+  // Why don't we use reduce instead of for loop? :-)
+  const totalProducts = Object.values(products).reduce(
+    (sum, quantity) => sum + quantity,
+    0
+  );
+
+  return totalProducts <= containerSize;
 }
 
-console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+// Tests
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
